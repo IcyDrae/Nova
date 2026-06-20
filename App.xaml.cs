cdb3f2fb0c0787a6409e2fe3daf9a6a30185dc42
IcyDrae <35934418+IcyDrae@users.noreply.github.com>
2026-06-20 15:00:42 +0200

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Nova
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
