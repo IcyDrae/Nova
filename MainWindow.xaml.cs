c80081ef8bef0a9b07fdea37e400e7069aba6296
IcyDrae <35934418+IcyDrae@users.noreply.github.com>
2026-06-21 15:10:30 +0200

    feature: add global hotkey(alt+space), gradient border on the search bar

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,57 @@
-﻿using System.Windows;
-using System.Windows.Input;
+﻿using Nova.Services;
+using System.Runtime.InteropServices;
+using System.Windows;
 using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace Nova
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         private NotifyIcon? _notifyIcon;
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(
+            IntPtr hWnd,
+            int id,
+            uint fsModifiers,
+            uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(
+            IntPtr hWnd,
+            int id);
+
+        private const int HOTKEY_ID = 9000;
+        private const uint MOD_ALT = 0x0001;
+        private const uint VK_SPACE = 0x20;
+
+        private IntPtr WndProc(
+            IntPtr hwnd,
+            int msg,
+            IntPtr wParam,
+            IntPtr lParam,
+            ref bool handled)
+        {
+            const int WM_HOTKEY = 0x0312;
+
+            if (msg == WM_HOTKEY)
+            {
+                ShowNova();
+
+                handled = true;
+            }
+
+            return IntPtr.Zero;
+        }
 
         public MainWindow()
         {
             InitializeComponent();
             InitializeSearchBox();
             InitializeNotifyIcon();
+
+            Loaded += MainWindow_Loaded;
         }
 
         private void InitializeSearchBox()
@@ -65,6 +104,32 @@ namespace Nova
             {
                 HideNova();
             }
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var Helper = new System.Windows.Interop.WindowInteropHelper(this);
+
+            RegisterHotKey(
+                Helper.Handle,
+                HOTKEY_ID,
+                MOD_ALT,
+                VK_SPACE);
+
+            var Source = System.Windows.Interop.HwndSource.FromHwnd(Helper.Handle);
+
+            Source.AddHook(WndProc);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            var Helper = new System.Windows.Interop.WindowInteropHelper(this);
+
+            UnregisterHotKey(
+                Helper.Handle,
+                HOTKEY_ID);
+
+            base.OnClosed(e);
         }
     }
 }
---
+++ src/Services/GlobalHotkeyService.cs
@@ -0,0 +1,9 @@
+using System.Runtime.InteropServices;
+
+namespace Nova.Services
+{
+    public class GlobalHotkeyService
+    {
+
+    }
+}
